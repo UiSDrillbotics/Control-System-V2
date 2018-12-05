decd0396edb87230ab00e1547011cdda28d20d85
magn00bs <magnus@steinsto.net>
2018-12-05 13:04:36 +0100

    Added file description

---
+++ init.py
@@ -0,0 +1,8 @@
+###############################################################
+#
+#   Initialize system with sensors and motors
+#
+###############################################################
+
+
+# Connect to data acquisition system
