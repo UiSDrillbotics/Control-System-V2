bfddc556cf219c4e312ba07d37e2b7530bcc405f
magn00bs <magnus@steinsto.net>
2019-01-14 08:41:44 +0100

    Structure improvements

---
+++ database/database.py
@@ -0,0 +1,6 @@
+class Database:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
---
+++ rig/rotation.py
@@ -0,0 +1,10 @@
+class Rotation:
+
+    def __init__(self):
+        self.rpm = 0
+
+    @rpm.setter
+    def rpm(self, rotation):
+        self.rpm = rotation
+
+    
