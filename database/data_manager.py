0a3b5986ca489298427472b47f01f7656b849339
magn00bs <magnus@steinsto.net>
2019-01-14 17:56:11 +0100

    Structure skeleton

---
+++ database/data_manager.py
@@ -0,0 +1,30 @@
+class DataManager:
+
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def init_database(self):
+        pass
+
+    def drop_database(self):
+        pass
+
+    def drop_table(self):
+        pass
+
+    def create_table(self):
+        pass
+
+    def push_to_database(self):
+        pass
+
+    def push_to_sql_buffer(self):
+        pass
+
+    
