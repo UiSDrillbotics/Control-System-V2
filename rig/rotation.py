0323d2579b20c56d23a0ac4857f300566fd1eff9
magn00bs <magnus@steinsto.net>
2019-01-14 18:45:31 +0100

    Structure skeleton

--- rig/rotation.py
+++ rig/rotation.py
@@ -3,8 +3,14 @@ class Rotation:
     def __init__(self):
         self.rpm = 0
 
+    @property
+    def rpm(self):
+        return self.rpm
+
     @rpm.setter
     def rpm(self, rotation):
         self.rpm = rotation
+
+    
 
     
